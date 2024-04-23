e3f6e5331e160bcc0f56d5929bffba85f11398c0
Himanshu241b <himanshu.chaudhary@avisoft.io>
2024-04-23 15:32:59 +0530

    spring project initialized

---
+++ src/test/java/com/example/HRMSAvisoft/HrmsAvisoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HRMSAvisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsAvisoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
