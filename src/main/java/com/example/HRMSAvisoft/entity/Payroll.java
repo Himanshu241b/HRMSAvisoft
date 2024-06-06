c57aed3031529f62271a7ec1a87b0a4addad6826
Himanshu241b <himanshu.chaudhary@avisoft.io>
2024-06-06 15:49:44 +0530

    add user api bug resolved

--- src/main/java/com/example/HRMSAvisoft/entity/Payroll.java
+++ src/main/java/com/example/HRMSAvisoft/entity/Payroll.java
@@ -1,18 +1,35 @@
-//package com.example.HRMSAvisoft.entity;
-//
-//
-//import jakarta.persistence.Entity;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@Entity
-//@AllArgsConstructor
-//public class Payroll {
-//
-//
-//}
+package com.example.HRMSAvisoft.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+public class Payroll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long payrollId;
+
+    private BigDecimal bonus;
+
+    private String payDate;
+
+//    private List<Deduction> deductionList;
+
+    private String payableAmount;
+
+}
